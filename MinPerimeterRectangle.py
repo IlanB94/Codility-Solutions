dd6d503c61637c8e83aa5500980548e72956ae1a
ilan <ilan>
2024-12-23 15:45:54 +0200

    Add another solution

---
+++ MinPerimeterRectangle.py
@@ -0,0 +1,10 @@
+#Time complexity O(sqrt(N)
+def solution(N):
+    minParaimeter = 2**31-1
+    x = (int)(math.sqrt(N)) + 1
+    for i in range(1,x):
+        if N % i == 0:
+            res = (int)(N / i)
+            temp = 2 * (res + i)
+            minParaimeter = min(minParaimeter,temp)
+    return minParaimeter
